1d4ad1ebad0af590a23312e78d216866b2c6ae40
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2024-03-10 01:56:29 +0800

    fix：解决打包时不支持lombok的问题

--- src/main/java/com/jankinwu/dto/PushMsgDTO.java
+++ src/main/java/com/jankinwu/dto/PushMsgDTO.java
@@ -7,7 +7,6 @@ import lombok.Data;
  * @description
  * @date 2024/3/9 2:59
  */
-@Data
 public class PushMsgDTO {
 
     private String text;
@@ -17,4 +16,60 @@ public class PushMsgDTO {
     private String fontFamily;
     private String type;
     private String avatarUrl;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(String fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public String getFill() {
+        return fill;
+    }
+
+    public void setFill(String fill) {
+        this.fill = fill;
+    }
+
+    public String getStroke() {
+        return stroke;
+    }
+
+    public void setStroke(String stroke) {
+        this.stroke = stroke;
+    }
+
+    public String getFontFamily() {
+        return fontFamily;
+    }
+
+    public void setFontFamily(String fontFamily) {
+        this.fontFamily = fontFamily;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
 }
